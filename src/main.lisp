3345f7689313314822d65040d2e5b6d9f97eaf05
Masahiro Nagata <opampg@gmail.com>
2019-09-22 19:56:09 +0900

    コネクションをスレッドで処理するようにしてみた

--- src/main.lisp
+++ src/main.lisp
@@ -17,16 +17,20 @@
       ;#+ccl `(:v4 nil :v6 nil)
       ))
 
+(defun response-to-client (conn)
+  (let ((client-addr (get-peer-address conn)))
+    (unwind-protect
+         (progn
+           (sleep 5)
+           (format (socket-stream conn) "~A" client-addr)
+           (force-output (socket-stream conn)))
+      (socket-close conn))))
+
 (defun start-server (&key (host "127.0.0.1") (port 9000))
   (let ((socket (socket-listen host port)))
     (unwind-protect (loop
-                       (let* ((conn (socket-accept socket :element-type 'character))
-                              (client-addr (get-peer-address conn)))
-                         (unwind-protect
-                              (progn
-                                (format (socket-stream conn) "~A" client-addr)
-                                (force-output (socket-stream conn)))
-                           (socket-close conn))))
+                       (let ((conn (socket-accept socket :element-type 'character)))
+                         (make-thread (lambda () (response-to-client conn)))))
       (progn
         (socket-close socket)))))
 
